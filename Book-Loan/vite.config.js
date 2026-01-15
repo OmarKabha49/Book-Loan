2215f7969841d0f938cd6faf7e2724139784a430
OmarKabha49 <Omar.Kabha1@msmail.ariel.ac.il>
2026-01-15 16:34:47 +0200

    feat: Initialize new React project with Vite, React Router DOM, TailwindCSS, and GitHub Pages deployment.

--- Book-Loan/vite.config.js
+++ Book-Loan/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true
-  }
+  // IMPORTANT: Replace 'book-loan-app' with your EXACT repository name
+  base: '/Book-Loan/',
 })
